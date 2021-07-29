2c5610fd3c5f60cc22deec1e67b11b53dc89735f
Matthew O'Neill <oneill_91@hotmail.co.uk>
2021-07-29 13:00:31 +0100

    documented existing functions removed the need to click over to the macro tab, as the macros are accessible but hidden.
    TODO: write the update function to take any changes and apply them roll20

--- content_scripts/macrosave.js
+++ content_scripts/macrosave.js
@@ -1,74 +1,91 @@
-if (typeof browser === "undefined") {
-    var browser = chrome;
-}
-(function() {
-	/**
+"use strict";
+(function () {
+  /**
 	 * Check and set a global guard variable.
 	 * If this content script is injected into the same page again,
 	 * it will do nothing next time.
 	 */
-	if (window.hasRun) {
-	  return;
-	}
-	window.hasRun = true;
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
 
-function populateLists(){
-	document.querySelector("#ui-id-5").click();
-	document.querySelectorAll("td.name").forEach(function(macro){macro.click()});
-	names = document.querySelectorAll("input.name")
-	bodies = document.querySelectorAll("textarea.macro")
+  function populateLists() {
+    // macros are stored in table elements with the class of name
+    // clicking on these elements opens the macros which we can then use
+    // to get the macros name and the body of the macro
+    document.querySelectorAll("td.name").forEach(function (macro) {
+      macro.click();
+    });
+    let names = document.querySelectorAll("input.name");
+    let bodies = document.querySelectorAll("textarea.macro");
+    return [names, bodies];
+  }
 
-	return [names, bodies]
-}
+  function retrieveMacros() {
+    // gets the names and bodies of the macros and stores them as
+    // key value pairs. Then clicks the cancel button on all of the open
+    // macro windows so as not to update them.
 
-function retrieveMacros (){
-	[names, bodies] = populateLists()
+    [names, bodies] = populateLists();
 
-	obj = {};
-	for (var i = 0; i<names.length; i++){
-		obj[names[i].value] = bodies[i].value
-	}
+    let obj = {};
+    for (var i = 0; i < names.length; i++) {
+      obj[names[i].value] = bodies[i].value;
+    }
 
-	buttons = document.querySelectorAll("button")
+    buttons = document.querySelectorAll("button");
 
-	buttons.forEach(function(button){
-		if (button.innerText == "Cancel"){
-			button.click()
-		}
-	}
-	)
+    buttons.forEach(function (button) {
+      if (button.innerText == "Cancel") {
+        button.click();
+      }
+    });
 
-	return obj
-}
+    return obj;
+  }
 
-function load_macros(){
-	console.log("loading macros")
-	browser.storage.sync.get(null)
-	.then(console.log)
-	.catch(console.log)
-}
+  function loadMacros() {
+    // loads all the macros from browser sync storage
+    // sending null retrieves all.
+    console.log("loading macros");
+    browser.storage.sync.get(null)
+      .then(console.log)
+      .catch(console.log);
+  }
 
-function getPlayerName(){
-	return document.querySelector("#player_displayname").value
-}
-browser.runtime.onMessage.addListener((message) => {
-	switch(message.command){
-		case "save":
-			obj = retrieveMacros()
-			character_name = getPlayerName()
-			console.log(obj, character_name)
+  function getPlayerName() {
+    // the player name is stored in a box with the ID player_displayname
+    return document.querySelector("#player_displayname").value;
+  }
+  browser.runtime.onMessage.addListener((message) => {
+    switch (message.command) {
+      case "save":
+        // with save we retreive the macros from the active player
+        // get their character name and store it as a keyvalue pair
+        // in the browsers sync storage. {Character: macros}
+        obj = retrieveMacros();
+        character_name = getPlayerName();
+        console.log(obj, character_name);
 
-			try {
-				browser.storage.sync.set({[character_name] : obj})
-				console.log("saved")
-			} catch (error) {
-				console.log("not saved")
-				console.log(`Unable to save ${error}`)
-			}
-			break;
-		case "load":
-			load_macros();
-			break;
-	}}
-)
-})()
+        try {
+          browser.storage.sync.set({ [character_name]: obj });
+          console.log("saved");
+        } catch (error) {
+          console.log("not saved");
+          console.log(`Unable to save ${error}`);
+        }
+        break;
+      case "load":
+        // loads macros from sync storage.
+        loadMacros();
+        break;
+      case "update":
+        // loads the macros from browser storage, if a macro with the same
+        // name exists, its contents are updated. If it doesn't, a new macro
+        // is created.
+        updateMacros();
+        break;
+    }
+  });
+})();
